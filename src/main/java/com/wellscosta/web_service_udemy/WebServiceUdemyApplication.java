9e4a8be9e1258ce5ca89db19eba756f34323c6b6
WellSCosta <wellscostaofc@gmail.com>
2024-08-30 09:33:58 -0300

    Project created

---
+++ src/main/java/com/wellscosta/web_service_udemy/WebServiceUdemyApplication.java
@@ -0,0 +1,13 @@
+package com.wellscosta.web_service_udemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServiceUdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServiceUdemyApplication.class, args);
+	}
+
+}
